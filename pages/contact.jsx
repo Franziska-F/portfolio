3caedffb225f817ae96964e16ee0a15b67ca988d
Franziska-F <f.fouchs@gmail.com>
2022-05-02 15:15:35 +0200

    Add personalized content

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Link from 'next/link';
+
 import Navbar from '../components/Navbar';
 
 function Contact() {
@@ -17,12 +17,9 @@ function Contact() {
             <h1 data-cy="pageTitle" className="title">
               Contact
             </h1>
-            <p className="description">
-              Incididunt in cillum magna occaecat nisi qui in exercitation
-              labore anim adipisicing amet irure tempor.
-            </p>
+            <p className="description">Please contact me:</p>
             <ul className="contact-links">
-              <li className="contact-item">Email: lutfhi@me.com</li>
+              <li className="contact-item">Email: f.fouchs@gmail.com</li>
               <li className="contact-item">Phone: +6212344321007</li>
             </ul>
           </div>
